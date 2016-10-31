993b5c22745efaaeb66db9d9cd42d1f066557dd3
KenRoss <kenn.ross@gmail.com>
2016-10-31 02:13:47 -0300

    Inital commit

---
+++ example-arrow-functions.js
@@ -0,0 +1,12 @@
+function add (a, b) {
+	return a + b;
+}
+
+var addStatement = (a, b) => {
+	return a + b;
+}
+
+var addExpression = (a, b) => a + b;
+
+console.log(addStatement(21, 3));
+console.log(addExpression(9, 8));
