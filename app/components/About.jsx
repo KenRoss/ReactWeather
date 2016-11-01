ce7aedfc3724d38170ecb228bb341f7b2ea78726
KenRoss <kenn.ross@gmail.com>
2016-11-01 20:12:24 -0300

    Update about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,10 +1,16 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var About = (props) => {
 	return (
 		<div>
-			<h3>About</h3>
-			<p>Welcome to the about page.</p>
+			<h1 className="text-center">About</h1>
+			<p>Welcome to the about page. This is an example app built while following a Udemy course.</p>
+			<ol>
+				<li>
+					<Link to="https://github.com/KenRoss/ReactWeather">GitHub page</Link>
+				</li>
+			</ol>
 		</div>
 	)
 };
