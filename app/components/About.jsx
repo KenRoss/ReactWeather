39bf9ed77cec184930cb6accf13dd31c94a7c8a6
KenRoss <kenn.ross@gmail.com>
2016-11-04 14:32:45 -0300

    Added CSS

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var About = (props) => {
 	return (
 		<div>
-			<h1 className="text-center">About</h1>
+			<h1 className="text-center page-title">About</h1>
 			<p>Welcome to the about page. This is an example app built while following a Udemy course.</p>
 			<ol>
 				<li>
